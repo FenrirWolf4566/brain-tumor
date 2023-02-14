baca577bae712d752ae42b87c2c3e83ca928d4ad
FenrirWolf4566 <bdezordo@gmail.com>
2023-02-14 12:04:10 +0100

    Create Crop

--- test_IA/Croping/crop.py
+++ test_IA/Croping/crop.py
@@ -3,12 +3,30 @@ import nibabel as nib
 import os
 from tqdm import tqdm
 import os
+import matplotlib.pyplot as plt
 
 #https://nipy.org/nibabel/nibabel_images.html
 
 # Example
 path="test_IA/Croping/test_flair.nii.gz"
 img = nib.load(path)
-print(img)
+#print(img)
+# Longueur largeur profondeur
 
 
+def cropping(img,size=90):
+    cropped_img = img.slicer[32:-32, ...]
+    print("Basic shape : ",img.shape)
+    print("New shape : ",cropped_img.shape)
+    return(cropped_img)
+
+
+def showSlice(img):
+    plot_nifti=img.get_fdata()
+    plot_nifti = plot_nifti[:,:,59]
+    plt.imshow(plot_nifti)
+    plt.show()
+
+
+cro=cropping(img)
+#showSlice(img)

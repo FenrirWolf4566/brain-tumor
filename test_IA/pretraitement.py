74c65e12599f1bdd0be6761f83cf1129350855ac
FenrirWolf4566 <bdezordo@gmail.com>
2023-02-14 08:41:03 +0100

    Fin : pretraitement

---
+++ test_IA/pretraitement.py
@@ -0,0 +1,10 @@
+from nilearn import plotting
+import pylab as plt
+import numpy as np
+import nibabel as nb
+import os
+from tqdm import tqdm
+import shutil
+
+#https://peerherholz.github.io/workshop_weizmann/data/image_manipulation_nibabel.html
+

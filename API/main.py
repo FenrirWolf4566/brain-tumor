54f6f37ac7a298279cf2025d8d7ba2b439af4b08
Léo Filoche <leofiloche.wk@gmail.com>
2023-02-15 13:56:52 +0100

    Première interraction avec API depuis le front

--- API/main.py
+++ API/main.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 import os
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")

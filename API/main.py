875223d366b23dfac03227513d9c1b8c12fae68a
lmorlotpinta <louis.morlot-pinta@etudiant.univ-rennes1.fr>
2023-02-27 11:50:34 +0100

    write en comment

--- API/main.py
+++ API/main.py
@@ -33,7 +33,7 @@ app.add_middleware(
 async def write_file(file:UploadFile= File(...)):
     file_path = os.path.join(os.getcwd(), file.filename)
     with open(file_path, "wb") as f:
-        f.write(file.file.read())
+        #f.write(file.file.read())
         return {"filename": file.filename}
      
 @app.get("/")
@@ -45,6 +45,21 @@ async def create_file_t1(file:UploadFile):
     fichiers_locaux.append(file)
     return await write_file(file)
 
+@app.post("/files/t2")
+async def create_file_t2(file: bytes = File()):
+    assert fichier_bon(file)
+    fichiers_locaux.append(file)
+    return {"file_size": len(file)}
+
+@app.post("/files/t1ce")
+async def create_file_t1ce(file: bytes = File()):
+    assert fichier_bon(file)
+    fichiers_locaux.append(file)
+    return {"file_size": len(file)}
+
+@app.post("/files/flair")
+async def create_file_flair(file: bytes = File()):
+    assert fichier_bon(file)
 
 def fichier_bon(file: UploadFile):
     return Path(file).suffix=='t1.nii.gz' or os.path.splitext(file)[1]=='t2.nii.gz' or os.path.splitext(file)[1]=='t1ce.nii.gz' or os.path.splitext(file)[1]=='flair.nii.gz'

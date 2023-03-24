1bf822a86a11e6b2ff1b5bede99c53d1823798a4
Léo Filoche <leofiloche.wk@gmail.com>
2023-03-24 23:17:42 +0100

    Connexion / Déconnexion opérationnel

---
+++ frontend/public/account.js
@@ -0,0 +1,25 @@
+function estConnecte(){
+    let token =  localStorage.getItem(SESSION_TOKEN);
+    return token !== null && token.length > 0;
+}
+async function whoami(){
+    const url = ROOT_URL+'account/me/';
+    const token = localStorage.getItem(SESSION_TOKEN);
+    console.log(token)
+   return await fetch(url, {method: 'GET',headers: {'Accept': 'application/json','Authorization': `Bearer `+token}}).then(response => response.json())
+}
+
+function triggerConnectedEvent(){
+    const event = new Event("connected");
+    document.dispatchEvent(event);
+}
+
+function triggerDisconnectedEvent(){
+    const event = new Event("disconnected");
+    document.dispatchEvent(event);
+}
+
+function seDeconnecter(){
+    localStorage.removeItem(SESSION_TOKEN);
+    triggerDisconnectedEvent();
+}

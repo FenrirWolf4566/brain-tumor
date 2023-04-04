cbc6192cb2dac871df31bfaca32a44dc2f440d78
lmorlotpinta <louis.morlot-pinta@etudiant.univ-rennes1.fr>
2023-04-04 17:05:55 +0200

    bdd API

--- API/bdd.py
+++ API/bdd.py
@@ -1,33 +1,42 @@
 import sqlite3
 
 # Établir une connexion à la base de données
-conn = sqlite3.connect('matable.db')
+def get_conn(database_name):
+    return sqlite3.connect(database_name)
+
+conn = sqlite3.connect('users.db')
 
 # Créer les tables
 conn.execute('DROP TABLE IF EXISTS comptes;')
 conn.execute('DROP TABLE IF EXISTS patients;')
-conn.execute('''CREATE TABLE movies (
-                id               INTEGER PRIMARY KEY AUTOINCREMENT,
-                id               TEXT NOT NULL,
-                username         TEXT,
-                full_name        TEXT,
-                email            TEXT,
-                hashed_password  TEXT
-              );''')
-conn.execute('''CREATE TABLE patients (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                nom VARCHAR(50),
-                prenom VARCHAR(50),
-              );''')
 
-# Insérer les données
-conn.execute('''INSERT INTO comptes (id, username, full_name, email, hashed_password)
-                VALUES ('1', 'johndoe', 'John Doe', 'johndoe@example.com', 'pwd_context.hash("bonjour")'),
-                       ('2', 'alicefontaine', Alice Fontaine, 'alicefontaine@example.com', 'pwd_context.hash("salut")');''')
+def create_movies_table(conn):
+    conn.execute('''CREATE TABLE movies (
+                    id               INTEGER PRIMARY KEY AUTOINCREMENT,
+                    id               TEXT NOT NULL,
+                    username         TEXT,
+                    full_name        TEXT,
+                    email            TEXT,
+                    hashed_password  TEXT
+                  );''')
 
-conn.execute('''INSERT INTO patients (nom, prenom)
-                VALUES ('Doe', 'John'),
-                       ('Fontaine', 'Alice');''')
+def create_patients_table(conn):
+    conn.execute('''CREATE TABLE patients (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    nom VARCHAR(50),
+                    prenom VARCHAR(50),
+                  );''')
 
-# Fermer la connexion
-conn.close()
+def insert_data_into_comptes_table(conn):
+    conn.execute('''INSERT INTO comptes (id, username, full_name, email, hashed_password)
+                    VALUES ('1', 'johndoe', 'John Doe', 'johndoe@example.com', 'pwd_context.hash("bonjour")'),
+                           ('2', 'alicefontaine', 'Alice Fontaine', 'alicefontaine@example.com', 'pwd_context.hash("salut")');''')
+
+def insert_data_into_patients_table(conn):
+    conn.execute('''INSERT INTO patients (nom, prenom)
+                    VALUES ('Doe', 'John'),
+                           ('Fontaine', 'Alice');''')
+
+def close_connection(conn):
+    conn.close()
+

07ad6eb38a28d2d0a2a54c04d13e2aaca413459a
lmorlotpinta <louis.morlot-pinta@etudiant.univ-rennes1.fr>
2023-04-04 15:58:53 +0200

    bdd

--- API/bdd.py
+++ API/bdd.py
@@ -8,30 +8,26 @@ conn.execute('DROP TABLE IF EXISTS comptes;')
 conn.execute('DROP TABLE IF EXISTS patients;')
 conn.execute('''CREATE TABLE movies (
                 id               INTEGER PRIMARY KEY AUTOINCREMENT,
-                title            TEXT NOT NULL,
-                primary_director TEXT,
-                year_released    INT,
-                genre            TEXT
+                id               TEXT NOT NULL,
+                username         TEXT,
+                full_name        TEXT,
+                email            TEXT,
+                hashed_password  TEXT
               );''')
 conn.execute('''CREATE TABLE patients (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 nom VARCHAR(50),
                 prenom VARCHAR(50),
-                age INT
               );''')
 
 # Insérer les données
-conn.execute('''INSERT INTO comptes (id, username, full_name, hashed_password)
-                VALUES ('1', 'johndoe', 1997, 'sci-fi'),
-                       ('Pirates of the Caribbean: The Curse of the Black Pearl', 'Gore Verbinski', 2003, 'fantasy'),
-                       ('Harry Potter and Goblet of Fire', 'Mike Newell', 2005, 'fantasy'),
-                       ('The Hobbit: An Unexpected Journey', 'Peter Jackson', 2012, 'fantasy'),
-                       ('Titanic', 'David Cameron', 1998, 'drame'),
-                       ('Intouchables', 'Olivier Nakache', 2011, 'comedie');''')
+conn.execute('''INSERT INTO comptes (id, username, full_name, email, hashed_password)
+                VALUES ('1', 'johndoe', 'John Doe', 'johndoe@example.com', 'pwd_context.hash("bonjour")'),
+                       ('2', 'alicefontaine', Alice Fontaine, 'alicefontaine@example.com', 'pwd_context.hash("salut")');''')
 
-conn.execute('''INSERT INTO patients (nom, prenom, age)
-                VALUES ('Doe', 'John', 42),
-                       ('Fontaine', 'Alice', 31);''')
+conn.execute('''INSERT INTO patients (nom, prenom)
+                VALUES ('Doe', 'John'),
+                       ('Fontaine', 'Alice');''')
 
 # Fermer la connexion
 conn.close()

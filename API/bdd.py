27d56f6e22d002b7400d86933bfe1f604af2e2c9
lmorlotpinta <louis.morlot-pinta@etudiant.univ-rennes1.fr>
2023-04-04 17:15:07 +0200

    delete API bdd fonctions inutiles

--- API/bdd.py
+++ API/bdd.py
@@ -10,8 +10,8 @@ conn = sqlite3.connect('users.db')
 conn.execute('DROP TABLE IF EXISTS comptes;')
 conn.execute('DROP TABLE IF EXISTS patients;')
 
-def create_movies_table(conn):
-    conn.execute('''CREATE TABLE movies (
+def create_comptes_table(conn):
+    conn.execute('''CREATE TABLE comptes (
                     id               INTEGER PRIMARY KEY AUTOINCREMENT,
                     id               TEXT NOT NULL,
                     username         TEXT,
@@ -19,18 +19,6 @@ def create_movies_table(conn):
                     email            TEXT,
                     hashed_password  TEXT
                   );''')
-
-def create_patients_table(conn):
-    conn.execute('''CREATE TABLE patients (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    nom VARCHAR(50),
-                    prenom VARCHAR(50),
-                  );''')
-
-def insert_data_into_comptes_table(conn):
-    conn.execute('''INSERT INTO comptes (id, username, full_name, email, hashed_password)
-                    VALUES ('1', 'johndoe', 'John Doe', 'johndoe@example.com', 'pwd_context.hash("bonjour")'),
-                           ('2', 'alicefontaine', 'Alice Fontaine', 'alicefontaine@example.com', 'pwd_context.hash("salut")');''')
 
 def insert_data_into_patients_table(conn):
     conn.execute('''INSERT INTO patients (nom, prenom)

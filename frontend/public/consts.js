47add10cd21da7aa93f820bc25c1359341d7ca03
Léo Filoche <leofiloche.wk@gmail.com>
2023-04-15 21:19:26 +0200

    Changement de ports pour Docker le front est au port 80 et le back au port 8000 ajout d'une zone de drag & drop création d'une page erreur 404

--- frontend/public/consts.js
+++ frontend/public/consts.js
@@ -1,6 +1,3 @@
 const SESSION_TOKEN ='session-token';
 const SESSION_END='expiration-token';
-// In dev mode
-// const ROOT_URL = 'http://127.0.0.1:8000/';
-// In docker mode
-const ROOT_URL = 'http://localhost:80/';
+const ROOT_URL = 'http://127.0.0.1:8000/';
